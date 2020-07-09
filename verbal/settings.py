d69c34452a788a31d0714d08b06f57f72e00fd96
ShaliniR8 <61011528+ShaliniR8@users.noreply.github.com>
2020-07-09 15:16:12 +0530

    Update settings.py

--- verbal/settings.py
+++ verbal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1ho443-8c!qo#(b4015kgwuf%ck=x+7hcdjp#4rie#f&qs29c_'
+SECRET_KEY = #key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
